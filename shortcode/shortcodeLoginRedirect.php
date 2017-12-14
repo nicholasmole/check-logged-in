3b36c2409bb413ad02b7ac2e1090754620caf07f
Nick M <onethousandmarkers@gmail.com>
2017-12-14 12:12:03 -0500

    fix shortcode already logged in

--- shortcode/shortcodeLoginRedirect.php
+++ shortcode/shortcodeLoginRedirect.php
@@ -1,10 +1,15 @@
 <?php
+
 add_shortcode( 'checkLoginRedirect', 'checkLoginRedirect' );
 
+include_once(ABSPATH . 'wp-includes/pluggable.php');
+
 function checkLoginRedirect($attr){
-    $url = ($attr["url"] == NULL) ? "" : $attr["url"];
-    wp_redirect( home_url().$url );
-    exit();
+    if ( !is_admin() && is_user_logged_in()  ) {
+        $url = ($attr["url"] == NULL) ? "" : $attr["url"];
+        wp_redirect( home_url().$url );
+        exit();
+    }
 }
 
 ?>
